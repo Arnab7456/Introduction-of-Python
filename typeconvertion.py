ae094b59c1a3b7dde85ed697881ae5c84c75f86c
Arnab7456 <arnabdas2753@gmail.com>
2023-03-08 11:41:21 +0530

    new

---
+++ typeconvertion.py
@@ -0,0 +1,10 @@
+brith_year = input("enter the birth year : ")
+# here we change the str class to int ==> type convertion
+Current_year = 2023-int(brith_year)
+print(Current_year)
+print(type(brith_year))
+print(type(Current_year))
+
+weight_lbs = input("weight (lbs) : ")
+weight_kg = float(weight_lbs) *0.45
+print(weight_kg)
